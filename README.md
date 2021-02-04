97e35b3ff1c7b5525715a6210b2c45f25ff7ba78
AK293 <aditya12murali@gmail.com>
2021-02-03 21:38:27 -0800

    n/a

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sift_implementation
 Implementation of the SIFT Image Descriptor
+
+Things to implement soon:
+1. Fix gaussian kernel (error: half_kernel_width not defined????)
+2. Fix bilinear interpolator
+3. Compute seed image
+4. Create necessary loops to generate images for each scale space
--- implementation.py
+++ implementation.py
@@ -17,14 +17,42 @@ sigma_min = 0.8
 bilinear_img = cv2.resize(img,None, fx = sqrt(2), fy = sqrt(2), interpolation = cv2.INTER_LINEAR)
 seed_sigma = 1/sigma_min* sqrt(pow(sigma_min,2)-pow(sigma_input,2))
 
-def gaussian_formula(x,y,sigma):
-    x_bar = sqrt(pow(x,2) + pow(y, 2))
-    num = exp(-1 * pow(x_bar,2)/(2 * pow(sigma,2)))
-    denom = 2 * pi * pow(sigma,2)
-    return num/denom
+#creating 1D gaussian kernels
 
-for i in range(1, oct_max):
-    interpixel_distance = pixel_min * pow(2,i-1)
-    sigma_first_part = interpixel_distance/interpixel_min * sigma_min
-    for j in range(1:spo + 2):
-        sigma = sigma * pow(2, j/spo)
+#notice that there is no denominator term
+#normalization will occur within the actual gaussian functions
+def modified_gaussian_formula(x,half_width, sigma):
+    num = exp(-1 * pow(x - half_width,2)/(2 * pow(sigma,2)))
+    return num
+
+
+sigma = sqrt(2) #for now, we will assign sigma = sqrt(2)
+def horizontal_gaussian(img, kernel_width = 3, sigma = sigma):
+    img_width,img_height,channels = img.shape
+    half_kernel_width = (kernel_width - 1)/2
+    print(half_kernel_width)
+    sum = 0
+    for k in range(0, channels):
+        for j in range(0, img_height):
+            for i in range(0, img_width):
+                img[i][j][k] = modified_gaussian_formula(i, half_kernel_width, sigma) 
+                sum += img[i][j][k]
+        for j in range(0, img_height):
+            for i in range(0, img_width):
+                img[i][j][k] = img[i][j][k]/sum
+    return img
+
+def vertical_gaussian(img, kernel_width = 3, sigma = sigma):
+    img_width,img_height,channels = img.shape
+    half_kernel_width = (kernel_width - 1)/2
+    print(half_kernel_width)
+    sum = 0
+    for k in range(0, channels):
+        for i in range(0, img_width): 
+            for j in range(0, img_height):
+                img[i][j] = modified_gaussian_formula(j, half_kernel_width, sigma)
+                sum += img[i][j][k]
+        for i in range(0, img_width):
+            for j in range(0, img_height):
+                img[i][j][k] = img[i][j][k]/sum
+    return img
