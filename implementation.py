36c66d9de182cdd1fd18c054d14729d050293952
AK293 <aditya12murali@gmail.com>
2021-02-13 16:06:30 -0800

    Working on Extraction of Candidate Keypoints

--- implementation.py
+++ implementation.py
@@ -16,43 +16,61 @@ sigma_min = 0.8
 
 bilinear_img = cv2.resize(img,None, fx = sqrt(2), fy = sqrt(2), interpolation = cv2.INTER_LINEAR)
 seed_sigma = 1/sigma_min* sqrt(pow(sigma_min,2)-pow(sigma_input,2))
+seed_img = cv2.GaussianBlur(bilinear_img, (0,0), seed_sigma/sqrt(2), cv2.BORDER_DEFAULT)
 
-#creating 1D gaussian kernels
+def sigma_formula(octave, scale):
+    return sigma_min/interpixel_min * sqrt(pow(2, 2*scale/spo) - pow(2, 2*(scale-1)/spo))
+def new_interpixel_value(octave):
+    return interpixel_min * exp(2, octave-1)
 
-#notice that there is no denominator term
-#normalization will occur within the actual gaussian functions
-def modified_gaussian_formula(x,half_width, sigma):
-    num = exp(-1 * pow(x - half_width,2)/(2 * pow(sigma,2)))
-    return num
+curr_img = seed_img[:]
+plt.imshow(seed_img,cmap = 'gray')
+plt.title('Octave 1, Scale 0: Seed Image'.format()), plt.xticks([]), plt.yticks([])
+plt.show()
 
+images = {} #keys are two-digit nummbers: 
+                #1st digit = octave #, 2nd digit = scale #
+                #values are images
+images[10] = seed_img
 
-sigma = sqrt(2) #for now, we will assign sigma = sqrt(2)
-def horizontal_gaussian(img, kernel_width = 3, sigma = sigma):
-    img_width,img_height,channels = img.shape
-    half_kernel_width = (kernel_width - 1)/2
-    print(half_kernel_width)
-    sum = 0
-    for k in range(0, channels):
-        for j in range(0, img_height):
-            for i in range(0, img_width):
-                img[i][j][k] = modified_gaussian_formula(i, half_kernel_width, sigma) 
-                sum += img[i][j][k]
-        for j in range(0, img_height):
-            for i in range(0, img_width):
-                img[i][j][k] = img[i][j][k]/sum
-    return img
+for i in range(1, spo + 3):
+    sigma_value = sigma_formula(1, i)
+    images[10+i] = cv2.GaussianBlur(curr_img, (0,0), sigma_value, cv2.BORDER_DEFAULT)
+    curr_img = images[10+i]
+    plt.imshow(curr_img,cmap = 'gray')
+    plt.title('Octave 1, Scale {}'.format(i)), plt.xticks([]), plt.yticks([])
+    plt.show()
 
-def vertical_gaussian(img, kernel_width = 3, sigma = sigma):
-    img_width,img_height,channels = img.shape
-    half_kernel_width = (kernel_width - 1)/2
-    print(half_kernel_width)
-    sum = 0
-    for k in range(0, channels):
-        for i in range(0, img_width): 
-            for j in range(0, img_height):
-                img[i][j] = modified_gaussian_formula(j, half_kernel_width, sigma)
-                sum += img[i][j][k]
-        for i in range(0, img_width):
-            for j in range(0, img_height):
-                img[i][j][k] = img[i][j][k]/sum
-    return img
+for j in range(2, oct_max + 1):
+    width, height = curr_img.shape[1] // 2, curr_img.shape[0] // 2
+    dim = (width, height)
+    print("The images from Octave {} will have dimensions of {}".format(j, (height, width)))
+    images[j*10] = cv2.resize(curr_img, dim)    
+    curr_img = images[j*10]    
+    plt.imshow(curr_img,cmap = 'gray')
+    plt.title('Octave {}, Scale 0'.format(j)), plt.xticks([]), plt.yticks([])
+    plt.show()
+    for i in range(1, spo+3):
+        sigma_value = sigma_formula(j, i)
+        images[j*10+i] = cv2.resize(curr_img, dim)   
+        curr_img = images[j*10+1]
+        plt.imshow(curr_img,cmap = 'gray')
+        plt.title('Octave {}, Scale {}'.format(j,i)), plt.xticks([]), plt.yticks([])
+        plt.show()
+
+#Difference of Gaussians
+"""
+Keys are two-digit numbers. 1st digit = octave
+#2nd digit = lower ones digit
+#i.e. the DOG between scale 2 and scale 1 for octave 6 will have a key of 61.
+"""
+dog = {}
+for octave in range(1, oct_max+1):
+    for scale in range(0, spo+2):
+        plt.imshow(images[octave*10+scale+1], cmap = 'gray')
+        plt.title('Octave {} Scale {}'.format(octave, scale+1))
+        plt.show()
+        plt.imshow(images[octave*10+scale], cmap = 'gray')
+        plt.title('Octave {} Scale {}'.format(octave, scale))
+        plt.show()
+        dog[octave*10+scale] = images[octave*10+scale+1] - images[octave*10+scale]
